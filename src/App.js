4aee8dfa4a6f7ebd5b8d5251296721c40a37f60f
House-chan <house3d@hotmail.com>
2022-12-25 19:45:20 +0700

    update margin

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import React, {useState} from "react"
 // import Payment from "./components/Payment"
 // import Privacy from "./components/Privacy"
 // import Purchase from "./components/Purchase"
-import Topic from "./components/AdminPage/Topic"
-import Promotion from "./components/AdminPage/Promotion"
+// import Topic from "./components/AdminPage/Topic"
+// import Promotion from "./components/AdminPage/Promotion"
 
-// import OrderStatus from "./components/OrderStatus/OrderStatus"
+import OrderStatus from "./components/OrderStatus/OrderStatus"
 
 function App() {
   
-  // const [active, setActive] = useState("Profile");
-  const [active, setActive] = useState("Promotion");
+  const [active, setActive] = useState("Profile");
+  // const [active, setActive] = useState("Promotion");
 
  
   return (
@@ -27,10 +27,10 @@ function App() {
       {active === "Privacy" && <Privacy />}
       {active === "Purchase" && <Purchase />} */}
 
-      <Topic setState={setActive}/>
-      {active === "Promotion" && <Promotion/>}
+      {/* <Topic setState={setActive}/>
+      {active === "Promotion" && <Promotion/>} */}
 
-      {/* <OrderStatus/> */}
+      <OrderStatus/>
     </div>
   );
 }
